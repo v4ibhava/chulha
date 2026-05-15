31bb36f7d87e29f3c33a1c85896dcdf9f16330f1
v4ibhava <ghodev0@gmail.com>
2026-05-15 14:14:16 +0530

    added render link in frontend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://chulha.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
